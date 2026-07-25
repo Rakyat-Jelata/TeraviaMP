c6102c05f9d2b809e44a064d46333b8d025dd5b4
Rakyat-Jelata <rjelata012@gmail.com>
2026-07-25 08:03:46 +0700

    Remove comment and add navbar include in header

--- includes/header.php
+++ includes/header.php
@@ -7,8 +7,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Marketplace Properti AI & Ads</title>
-    <!-- Link CSS Global & Responsif -->
     <link rel="stylesheet" href="/assets/css/style.css">
 </head>
 <body>
-  
+    <!-- Munculkan Navbar Desktop -->
+    <?php include __DIR__ . '/navbar-desktop.php'; ?>
